d2a3870fe9caf2d045a29594461202023165abf1
birdi <egor.birdi7@gmail.com>
2020-03-14 00:57:05 +0300

    feat: postgres db backend

--- meadow/meadow/settings.py
+++ meadow/meadow/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = "meadow.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": os.path.join(BASE_DIR, "db.sqlite3")}}
+DATABASES = {"default": {"ENGINE": "django.db.backends.postgresql", "HOST": "localhost", "USER": "postgres", "PASSWORD": "", "NAME": "meadow"}}
 
 
 # Password validation
