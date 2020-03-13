8e83a568e032a6093814bd7a7d0309fef39dec7c
Alina <a.paukova@innopolis.university>
2020-03-13 23:02:01 +0300

    fix: lock-file and possible fix of install_python

--- setup.d/install_python.sh
+++ setup.d/install_python.sh
@@ -2,7 +2,7 @@ echo "Installing python3.8..."
 apt update
 apt install -y software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
-apt install -y python3.8 python3.8-venv python3-pip
+apt install -y python3.8 python3-pip python3.8-venv
 echo "Python3.8 is accessible by python3.8 command. Running python3.8 --version: "
 python3.8 --version
 sleep 5
