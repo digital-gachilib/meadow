eabaeee0d8b9cb86327f91006f88a7a823b4c736
Egor <Birdi7@users.noreply.github.com>
2020-03-14 18:27:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-for i in "$@"; do
-    case $i in
-    -n=* | --name=*)
-        DIR_NAME="${i#*=}"
-        ;;
-    esac
-done
-
 git clone --progress git@github.com:digital-gachilib/meadow.git &&
     cd meadow && sudo bash init.d/install_all.sh
