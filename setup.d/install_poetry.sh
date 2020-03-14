154c6b7b0a94e44bee7dab42d0d55057bdc4beda
Egor <Birdi7@users.noreply.github.com>
2020-03-14 13:21:32 +0300

    Update install_poetry.sh

--- setup.d/install_poetry.sh
+++ setup.d/install_poetry.sh
@@ -1,5 +1,5 @@
 echo "installing poetry..."
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3.8
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
 echo "poetry is installed!"
 . $HOME/.poetry/env
 poetry --version
