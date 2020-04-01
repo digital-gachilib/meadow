bfaa74805d3f79f938c46c2ce22d718d1903c0d6
birdi <egor.birdi7@gmail.com>
2020-04-01 22:31:01 +0300

    fix: postgres running script

--- init.d/run_postgres.sh
+++ init.d/run_postgres.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run --name=meadow_db --rm -d -v dmd_postgres_data:/var/lib/postgresql/data -p 5432:5432 postgres
+docker run --name=meadow_db --rm -d -v dmd_postgres_data:/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_HOST_AUTH_METHOD=trust postgres
 # default username: postgres
 # default password: '' or 'postgres'
