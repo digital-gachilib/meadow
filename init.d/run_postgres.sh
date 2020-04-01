89a97f42ae89a235cf3fe25519af0eea18995179
birdi <egor.birdi7@gmail.com>
2020-04-01 19:00:53 +0300

    chore: reword

--- init.d/run_postgres.sh
+++ init.d/run_postgres.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker run --name=meadow_db --rm -d -v dmd_postgres_data:/var/lib/postgresql/data -p 5432:5432 postgres
 # default username: postgres
-# default password: ''/'postgres'
+# default password: '' or 'postgres'
