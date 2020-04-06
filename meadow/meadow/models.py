3d235c3e872ff8f7d08021f75d4ab3c8bd531a6e
birdi <egor.birdi7@gmail.com>
2020-04-06 18:13:36 +0300

    chore: add Meta to models

--- meadow/meadow/models.py
+++ meadow/meadow/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 
 class BookAuthor(models.Model):
+    class Meta:
+        verbose_name = "Book author"
+        verbose_name_plural = "Book authors"
+
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=30)
 
 
 class Book(models.Model):
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+
     title = models.CharField(max_length=200)
     isbn_10 = models.IntegerField
     isbn_13 = models.CharField(max_length=15)
