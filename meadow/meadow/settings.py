f29a61136ae888c53d25e9bbb33a364df9f53aad
JottoWorol <43534535+JottoWorol@users.noreply.github.com>
2020-04-06 12:04:17 +0300

    Login logic and requirements.txt update (#3)
    * Login logic and requirements.txt update
    
    add djungorestframework to requirements.txt
    created view for login
    created authorization token with login:bossofthegym pass:dungeonmaster
    
    * Update requirements.txt
    
    Co-Authored-By: Egor <Birdi7@users.noreply.github.com>
    
    * Updated urls.py
    Updated (cleared) views.py
    
    Co-authored-by: Egor <Birdi7@users.noreply.github.com>

--- meadow/meadow/settings.py
+++ meadow/meadow/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "meadow",
     "corsheaders",
 ]
 
@@ -101,6 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": ["rest_framework.authentication.TokenAuthentication",],
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
