fa1d274409b1d39ac7f9675a8709b76347221ffc
birdi <egor.birdi7@gmail.com>
2020-04-06 20:38:48 +0300

    fix: on empty title return all books

--- meadow/meadow/utils/book_searcher.py
+++ meadow/meadow/utils/book_searcher.py
@@ -1,7 +1,12 @@
+from typing import List
+
 from meadow.models import Book
 
 
-def search_by_title(title: str):
+def search_by_title(title: str) -> List[Book]:
+    if not title:
+        return list(Book.objects.all())
+
     books = []
     for book in Book.objects.all():
         if book.title.lower().count(title) > 0:
