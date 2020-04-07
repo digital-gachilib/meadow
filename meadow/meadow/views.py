7ab84304438562af82b5d7b3500831e3a4b67a9d
birdi <egor.birdi7@gmail.com>
2020-04-07 14:05:57 +0300

    chore: black

--- meadow/meadow/views.py
+++ meadow/meadow/views.py
@@ -1,7 +1,6 @@
 from django.http import JsonResponse
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.decorators import (api_view, authentication_classes,
-                                       permission_classes)
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 
