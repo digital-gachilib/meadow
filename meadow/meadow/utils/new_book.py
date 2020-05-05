a15defdc056cc0bf684e423220ad12f9096c9c61
JottoWorol <43534535+JottoWorol@users.noreply.github.com>
2020-05-05 19:21:36 +0300

    Added book creation API endpoint
    * feat: added book creation view
    
    * added new_book.py util

---
+++ meadow/meadow/utils/new_book.py
@@ -0,0 +1,8 @@
+from meadow.models import Book
+from meadow.models import BookAuthor
+
+
+def create_new_book(titl: str, link: str, descr: str, author_first: str, author_last: str):
+    a = BookAuthor(first_name=author_first, last_name=author_last)
+    a.save()
+    Book(title=titl, download_link=link, description=descr, author=a).save()
