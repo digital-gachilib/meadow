b36939e657f623c3d51210b1f97b38c56ea088ce
JottoWorol <43534535+JottoWorol@users.noreply.github.com>
2020-05-05 19:11:40 +0300

    Update new_book.py

--- meadow/meadow/utils/new_book.py
+++ meadow/meadow/utils/new_book.py
@@ -2,7 +2,7 @@ from meadow.models import Book
 from meadow.models import BookAuthor
 
 
-def create_new_book(link: str, descr: str, author_first: str, author_last: str):
+def create_new_book(titl:str, link: str, descr: str, author_first: str, author_last: str):
     a = BookAuthor(first_name=author_first, last_name=author_last)
     a.save()
-    Book(download_link=link, description=descr, author=a)
+    Book(title=titl, download_link=link, description=descr, author=a)
